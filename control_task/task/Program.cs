239a2615f6010f94d783be99d9f7908ddaba03bb
Leonid Maslov <mlenkei@mail.ru>
2023-04-08 12:52:26 +0300

    task_control_2

--- control_task/task/Program.cs
+++ control_task/task/Program.cs
@@ -5,4 +5,19 @@
 // При решение не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 
